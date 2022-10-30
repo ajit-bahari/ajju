d7af2b9e98c66e07fe33e066fdc7e6170cbef7f8
ajit-bahari <91064765+ajit-bahari@users.noreply.github.com>
2022-10-30 21:57:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Learner","Noob Coder", "Editor", "ajju"],
+        strings: ["Learner","Noob Coder", "Editor","ajju"],
         typeSpeed: 100,
         backSpeed: 60,
         cursorChar: ' |',
